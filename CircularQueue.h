2abb65480a09ca532c21cfeb158d69661d5c6b94
Vítor Teixeira <vitor_filipe_@hotmail.com>
2014-05-26 14:37:24 +0100

    Finished

--- CircularQueue.h
+++ CircularQueue.h
@@ -4,16 +4,12 @@
 #include <semaphore.h>
 #include <sys/types.h>
 #include <sys/mman.h>
-
 //------------------------------------------------------------------------------------------ 
 // Type of the circular queue elements 
- 
 typedef unsigned long QueueElem; 
- 
 //------------------------------------------------------------------------------------------ 
 // Struct for representing a "circular queue" 
 // Space for the queue elements will be allocated dinamically by queue_init() 
- 
 typedef struct 
 { 
  QueueElem *v; // pointer to the queue buffer
@@ -24,67 +20,51 @@ typedef struct
  sem_t full; // producer-consumer paradigm 
  pthread_mutex_t mutex; 
 } CircularQueue; 
- 
 //------------------------------------------------------------------------------------------ 
 // Allocates space for circular queue 'q' having 'capacity' number of elements 
 // Initializes semaphores & mutex needed to implement the producer-consumer paradigm 
 // Initializes indexes of the head and tail of the queue 
 // TO DO BY STUDENTS: ADD ERROR TESTS TO THE CALLS & RETURN a value INDICATING (UN)SUCESS 
- 
 int queue_init(CircularQueue **q, unsigned int capacity) { 
 	*q = (CircularQueue *) malloc(sizeof(CircularQueue)); 
-
 	if (sem_init(&((*q)->empty), 0, capacity) != 0)
 		return -1; 
 	if (sem_init(&((*q)->full), 0, 0) != 0)
 		return -1; 
 	if (pthread_mutex_init(&((*q)->mutex), NULL) != 0)
 		return -1; 
-
 	(*q)->v = (QueueElem *) malloc(capacity * sizeof(QueueElem)); 
 	(*q)->capacity = capacity; 
 	(*q)->first = 0; 
 	(*q)->last = 0; 
-
 	return 0;
 } 
-
 //------------------------------------------------------------------------------------------ 
 // Inserts 'value' at the tail of queue 'q' 
 void queue_put(CircularQueue *q, QueueElem value) { 
 	sem_wait(&q->empty);
 	pthread_mutex_lock(&q->mutex);
-
 	q->v[q->last] = value;
-
 	q->last++;
 	if(q->last >= q->capacity)
 		q->last = 0;
-
 	pthread_mutex_unlock(&q->mutex);
 	sem_post(&q->full);
 } 
-
 //------------------------------------------------------------------------------------------ 
 // Removes element at the head of queue 'q' and returns its 'value' 
 QueueElem queue_get(CircularQueue *q) {
 	QueueElem temp;
-
 	sem_wait(&q->full);
 	pthread_mutex_lock(&q->mutex);
-
 	temp = q->v[q->first];
-
 	q->first++;
 	if(q->first >= q->capacity)
 		q->first = 0;
-
 	pthread_mutex_unlock(&q->mutex);
 	sem_post(&q->empty);
-
 	return temp;
 } 
-
 //------------------------------------------------------------------------------------------ 
 // Frees space allocated for the queue elements and auxiliary management data 
 // Must be called when the queue is no more needed 
--- primes.c
+++ primes.c
@@ -7,8 +7,9 @@
 #include <math.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <time.h>
+#include <stdlib.h>
 #include "CircularQueue.h"
-
 #define queueSize 10
 
 unsigned long N; //maximum number
@@ -17,70 +18,94 @@ pthread_mutex_t primeListMut;
 QueueElem* primeNumbers;
 unsigned long primeNumbersSize;
 unsigned long arrayIndex;
-
+//------------------------------------------------------------------------------------------
+//Shows all prime numbers that the primeNumbers list has. Makes a verification. A random 
+//  number between 0 and the size of the list is chosen. That number represents the index in 
+//  primes list of the number that is going to be verified. Then if the quocient of
+//  that prime number with all numbers between 2 and that number(excludind itself) equals
+//  0, that number isn't prime. 
 void showPrimes() {
-
-	int i;
-	for(i = 0; i < primeNumbersSize; i++) {
-		if(i == primeNumbersSize - 1) {
+	unsigned long i,randPrime;
+	int verification=0;
+	QueueElem rPrime;
+	randPrime = rand() % (arrayIndex);
+	printf("\nThe prime numbers from 2 to %lu are:\n", N);
+	for(i = 0; i < arrayIndex; i++) {
+		if(i==randPrime)
+			rPrime=primeNumbers[i];
+		if(i == arrayIndex - 1) {
 			printf("%lu. \n", primeNumbers[i]);
 		}
 		else {
-			printf("%lu, \n", primeNumbers[i]);
+			printf("%lu, ", primeNumbers[i]);
 		}
 	}
+	for(i = 2; i < rPrime; i++) {
+		if(rPrime % i==0)
+		{
+			printf("\nVerification complete: %lu, isn't a prime number!!!\n", rPrime);
+			verification=1;
+			break;
+		}
+	}
+	if(verification==0)
+			printf("\nVerification complete: %lu, is a prime number!!!\n", rPrime);
 }
-
-
+//------------------------------------------------------------------------------------------
+// Includes in primeNumbers list the QueueElem that is been passed as a parameter. And updates
+//   all variables needed.
 void addPrimesToList(QueueElem prime) {
-	
 	pthread_mutex_lock(&primeListMut);
-	
 	primeNumbers[arrayIndex] = prime;
 	arrayIndex += 1;
 	primeNumbersSize+=1;
-	
 	pthread_mutex_unlock(&primeListMut);
-	
 }
+//------------------------------------------------------------------------------------------
+//Function that all threads(excluding the initThread) will use. This fuction checks if the 
+//   front value of the circular queue, that as been passed as a parameter, is greater than
+//   sqrt(N). If so, all values of the circular list are prime and are put in primeNumbers list.
+//   Otherwise, all values of the circular queue will be analised, and all numbers that are multiple
+//   of the first number of the queue will not be included in a new circular queue that will be passed
+//	 to the new thread that will be created.
 
 void *filterThreadFunc(void *arg) {
-	pthread_mutex_lock(&primeListMut);
 	CircularQueue* q = arg;
 	QueueElem elem = queue_get(q);
 	QueueElem first = elem;
+	addPrimesToList(first);
 	if(first  > (int) sqrt(N)){
-		addPrimesToList(first);
-		do
-		{
-			elem = queue_get(q);
+		elem = queue_get(q);
+		while(elem != 0) {
+			
 			addPrimesToList(elem);
-		} while(elem != 0);
+			elem = queue_get(q);
+		}
 		sem_post(&canTerminate);
 	}
-	else {
+	else {		
 		pthread_t filterThreadId;
 		CircularQueue* temp;
 		queue_init(&temp, queueSize);
 		if(pthread_create(&filterThreadId, NULL, filterThreadFunc, (void*) temp)) {
 			printf("Thread error.");
-		}
+		}	
 		do {
 			elem = queue_get(q);
 			if(elem % first != 0)
 				queue_put(temp, elem);
 		} while (elem != 0);
 		queue_put(temp, 0);
-		addPrimesToList(first);
 	}
-
-	pthread_mutex_unlock(&primeListMut);
 	queue_destroy(q);
 	return NULL;
 }
-
+//------------------------------------------------------------------------------------------
+// Function that only initThread use. This thread puts number 2 in the primeNumbers list. If N
+// 	 (parameter thats passed by user) equals 2, this thread can terminate and no other thread is 
+//   created. If not, a new thread is created and a new circular queue(that will be passed as a
+//   parameter) is initialized with all numbers(starting in 3), that aren't multiple of 3. 
 void *initThreadfunc(void *arg) {
-	
 	pthread_t filterThreadId;
 	CircularQueue *q;
 	queue_init(&q, queueSize);
@@ -94,30 +119,36 @@ void *initThreadfunc(void *arg) {
 			printf("Thread error.");
 		}
 		int i;
-		for(i = 3; i < N; i+=2) {
+		for(i = 3; i <= N; i+=2) {
 			queue_put(q, i);
-			printf("FODASSE");
 		}
 		queue_put(q, 0);
 	}	
 	return NULL;
 }
-	
+//------------------------------------------------------------------------------------------
+// Auxiliar function that is used in qsort, that compares which is the greater number between
+//    to given numbers.
 int compare(const void * a, const void * b)
 {
 		return ( *(QueueElem*)a - *(QueueElem*)b );
 }
-
+//------------------------------------------------------------------------------------------
+// A verification of the input parameter is done. If N is lower than 2 or isn't an integer
+//   program will end with a error message. Global variables are also initialized. InitiThread 
+//   is created. At the end of the, primeNumbers list is sorted and then all prime numbers are
+//   shown.
 int main(int argc, char *argv[]) {
-	
 	pthread_t initThreadId;
+	srand (time(NULL));
 	arrayIndex = 0;
 	N = strtol(argv[1], NULL, 10);
 	if(N < 2) {
-		sem_post(&canTerminate);
+		printf("Wrong input. Input must be an integer(2 or higher).\n");
+		return 0;																																																																																																																	
 	}
-	primeNumbersSize = 1.2*(N/log(N))*sizeof(QueueElem);
-	primeNumbers = (QueueElem*) malloc (sizeof(QueueElem) * 1.2 * N / log(N));
+	primeNumbersSize = ceil(1.2*(N/log(N))) +1;
+	primeNumbers = (QueueElem*) malloc (sizeof(QueueElem) * primeNumbersSize);
 	if(sem_init(&canTerminate, 0, 0) != 0) {
 		return -1;
 	}
@@ -128,13 +159,8 @@ int main(int argc, char *argv[]) {
 	if(pthread_create(&initThreadId, NULL, initThreadfunc, NULL)) {
 		printf("Error creating thread");
 	}
-	printf("FODASSE1");
 	sem_wait(&canTerminate);
-	
-	qsort(primeNumbers, primeNumbersSize, sizeof(QueueElem), compare);
-	
+	qsort(primeNumbers, arrayIndex, sizeof(QueueElem), compare);
 	showPrimes(primeNumbers);
-	
 	return 0;
-	
 }
